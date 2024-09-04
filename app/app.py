fb87ea0c12161aa027c525fb3fe14016917589af
Can yildiz <can.yildiz.1386@gmail.com>
2024-09-04 13:10:06 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -508,4 +508,4 @@ def purchase_nft():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True,port=9080,host="0.0.0.0")
+    app.run(debug=False,port=8000,host="0.0.0.0")
